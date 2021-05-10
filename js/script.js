7648ce8ecca3508729a8f65402700e1004c8bfeb
Ugo <success.us7@gmail.com>
2021-05-10 15:58:29 +0100

    Having browser issues

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var sharinghan = document.getElementById("main_radius");
 var themeSong = document.getElementById("theme-song");
 themeSong.play();
 
-themeSong.addEventListener("play", function () {
+themeSong.addEventListener("play", (event) => {
        sharinghan.removeAttribute("style");
 })
 

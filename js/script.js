eac5334e8cf9d21cb4c87e895913599d75c6393a
Ugo <success.us7@gmail.com>
2021-05-10 16:03:05 +0100

    Trying out other media events

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var sharinghan = document.getElementById("main_radius");
 var themeSong = document.getElementById("theme-song");
 themeSong.play();
 
-themeSong.addEventListener("play", (event) => {
+themeSong.addEventListener("loadedmetadata", (event) => {
        sharinghan.removeAttribute("style");
 })
 
